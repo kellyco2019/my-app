8a3e670c9ad16ca1c1754acf633b375a625727c7
kellyco2019 <kellyc@evollve.co>
2020-10-18 16:09:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Hola mundo</h1>
+    <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+    </div>
+    
+  );
+}
+
+export default App;
